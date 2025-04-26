1ca7e68ee7651bf7eb4eda69c2dea6b67b81d527
Leonid Pershin <lpershin@syntellect.ru>
2025-04-26 09:52:58 +0300

    add int vector

---
+++ Nez.Portable/Math/Vector2Int.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nez
+{
+    public class Vector2Int
+    {
+		public int X { get; set; }
+		public int Y { get; set; }
+		public Vector2Int(int x, int y)
+		{
+			X = x;
+			Y = y;
+		}
+		public static Vector2Int Zero = new Vector2Int(0, 0);
+	}
+}
